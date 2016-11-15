cbcb53adcb27d83709b0bda465bd8dd1b46e78bc
Terence Lee <hone02@gmail.com>
2016-11-15 17:32:31 -0600

    staging bucket

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -37,7 +37,7 @@ class LanguagePack::Ruby < LanguagePack::Base
 
   def initialize(build_path, cache_path=nil)
     super(build_path, cache_path)
-    @fetchers[:mri]    = LanguagePack::Fetcher.new(VENDOR_URL, @stack)
+    @fetchers[:mri]    = LanguagePack::Fetcher.new("#{VENDOR_URL}-staging", @stack)
     @fetchers[:rbx]    = LanguagePack::Fetcher.new(RBX_BASE_URL, @stack)
     @node_installer    = LanguagePack::NodeInstaller.new(@stack)
     @jvm_installer     = LanguagePack::JvmInstaller.new(slug_vendor_jvm, @stack)
